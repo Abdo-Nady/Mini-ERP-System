871d79b99b01c71e3ead152bbfdbff3110c656e7
Nady <nady.20545@gmail.com>
2025-12-10 19:10:21 +0200

    add customer model in adman page

--- customer/admin.py
+++ customer/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Customer
 
 # Register your models here.
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name','phone' ,'email' , 'address' , 'opening_balance']
+    search_fields = ['name', 'email']
