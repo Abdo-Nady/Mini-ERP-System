e232812013a44aaf1d4fbe8f26f38942b2805252
Nady <nady.20545@gmail.com>
2025-12-10 19:12:14 +0200

    add products  model in adman page

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Product
 
 # Register your models here.
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'sku','category','cost_price',    'selling_price', 'stock' ,'image']
+    search_fields = ['name', 'sku' ,'category']
