03e501f80f7c63c72ac7ed7c51c665816260f7d2
Nady <nady.20545@gmail.com>
2025-12-10 19:15:17 +0200

    add sales order  models to adman page

--- sales_order/admin.py
+++ sales_order/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import SalesOrder, SalesOrderLine, StockMovementLog
 
 # Register your models here.
+
+@admin.register(SalesOrder)
+class SalesOrderAdmin(admin.ModelAdmin):
+    list_display = ['order_number', 'customer','order_date','created_by', 'status', 'total_amount']
+    list_filter = ['status', 'order_date']
+
+@admin.register(SalesOrderLine)
+class SalesOrderLineAdmin(admin.ModelAdmin):
+    list_display = ['order', 'product', 'qty', 'price', 'line_total']
+    list_filter = ['order', 'product']
+
+@admin.register(StockMovementLog)
+class StockMovementLogAdmin(admin.ModelAdmin):
+    list_display = ['product', 'qty', 'user', 'timestamp']
+    list_filter = ['product', 'user']
