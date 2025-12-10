d262b6add073a8d57162796aff7c8c627e9de24d
Nady <nady.20545@gmail.com>
2025-12-10 20:48:18 +0200

    created some migrations

--- accounts/urls.py
+++ accounts/urls.py
@@ -10,6 +10,3 @@ urlpatterns = [
     path("logout/", LogoutView.as_view(), name="logout"),
 ]
 
-
-#    "refresh": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoicmVmcmVzaCIsImV4cCI6MTc2NTk5MDgzMywiaWF0IjoxNzY1Mzg2MDMzLCJqdGkiOiI0NjY2ZWZkNjhiOTQ0ODE2ODY1Yzk4ZjIzMTJjZTkxOSIsInVzZXJfaWQiOiIzIn0.m0e7YHIvY1WqPukfgoXQwAgJ7jNuJ2TJdYR5mXXq940",
-#     "access": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzY1Mzg3ODMzLCJpYXQiOjE3NjUzODYwMzMsImp0aSI6IjFjOGM1ODNhNmFkMTRjZTY5NzgwMjFlN2E1M2UwY2IxIiwidXNlcl9pZCI6IjMifQ.YTPP0gQNklqrZJrpb3pPlCa8x7ZwyBZbtMG-ESrGxwE
