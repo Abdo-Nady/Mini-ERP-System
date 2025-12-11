c8b7fff6091be2982b68fde9225d9b1d206ddb8c
Nady <nady.20545@gmail.com>
2025-12-11 02:19:30 +0200

    add pagination for products

--- products/views.py
+++ products/views.py
@@ -2,8 +2,13 @@ from rest_framework import viewsets
 from .models import Product
 from .serializers import ProductSerializer
 from .permissions import IsAdminOrReadOnly
+from rest_framework.pagination import PageNumberPagination
 
 
+class ProductPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
