c7048464dcafd2ad050e9d49af3f8c58b1f212b3
Nady <nady.20545@gmail.com>
2025-12-11 02:30:36 +0200

    add small category filter for products in the backend

--- products/views.py
+++ products/views.py
@@ -16,3 +16,11 @@ class ProductViewSet(viewsets.ModelViewSet):
     filter_backends = [filters.SearchFilter]
     search_fields = ['sku', 'name', 'category',]
     pagination_class = ProductPagination
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        # filter by category
+        category = self.request.query_params.get('category')
+        if category:
+            qs = qs.filter(category=category)
+        return qs
